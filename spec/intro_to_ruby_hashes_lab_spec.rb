63031db215ad7b8703b2a24a670fc6f03f107e05
Sean Beach <sean.beach1@yahoo.com>
2019-12-04 04:12:36 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -58,8 +58,9 @@ describe "updating data in a hash" do
     end
 
     it "if key is not present, returns a hash with the provided key assigned to the value of 1" do
-      expect(update_counting_hash({},'hello')).to eq({'hello' => 1})
-      expect(update_counting_hash({total: 5},:age)).to eq({total: 5, age: 1})
+      update_counting_hash = {
+        :age = 5
+      }
     end
 
     it "if key is present, returns a hash where the key's value is incremented by 1" do
